8edb7d3f868da7254c26d30f4883e2d03f36deeb
llj098 <llj098@gmail.com>
2011-12-07 07:38:18 -0800

    add share memory and fork code

--- proxy.c
+++ proxy.c
@@ -1,14 +1,61 @@
-/*author liulijin<llj098@gmail.com>*/ 
+/*
+ * Copyrtight liulijin<llj098@gmail.com>
+ */ 
 
 #include <stdio.h> 
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include "config.h"
 
 
+int 
+spawn_worker()
+{
+  int i = 0;
+
+  for(;i<4;i++){
+
+    int p = fork();
+
+    if(p < 0) {
+	printf("%s","forkerror");
+    }
+    else if(p == 0){/*child*/
+      printf("%d\n",getpid());
+    }
+    else{/*parent*/
+      printf("%d\n",getpid());
+    }
+  }
+
+  return 1;
+
+}
+
+int 
+create_shm()
+{
+  int p = shmget(IPC_PRIVATE,sizeof(4),0600/*user read&writer*/);
+  void* ptr;
+  if(p>0) {
+    printf("shm created, id %d\n",p);
+    ptr = shmat(p,0,0);
+    ptr = "123";
+    printf("shmat %lx,%s",ptr,ptr);
+  }
+  
+  return p;
+}
+
 int start()
 {
+  return create_shm();
+  return spawn_worker();
+
+
   int s = 1;
   int listen_fd =socket(AF_INET,SOCK_STREAM,0);
 
@@ -22,16 +69,17 @@ int start()
   //addr1.sin_addr.s_addr = 
 
 	
-  s =bind(listen_fd, 
-	  (struct sockaddr*)&addr1,
-	  sizeof(addr1)
-  );
+  s = bind(listen_fd, 
+	   (struct sockaddr*)&addr1,
+	   sizeof(addr1)
+	   );
 
 
   if(s < 0)
     return -1;
 
   listen(listen_fd,5);
+
 
 	
   return s;
@@ -41,7 +89,9 @@ int
 main(char* args,int len)
 {
   int s = start();
-  printf("%s\n%d","STARTED",s);
+  char st[2];
+  //printf("%s\n%d","STARTED",s);
+  scanf("%s",&st);
 }
 
 
