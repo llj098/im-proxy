a477e23914675e643f860a04cc8cb29915f821d5
llj098 <llj098@gmail.com>
2011-12-26 11:11:02 -0800

    fix hashtable bugs,still need to refact

--- hashtable.h
+++ hashtable.h
@@ -2,8 +2,10 @@
 #define _HASHTABLE_H_
 
 #define HT_INIT_SIZE 16
-#include "sysinc.h"
-#include "mempool.h"
+#include "proxy.h"
+
+typedef void ht_clean(void* d);
+
 
 typedef struct ht_key_s{
   uint32_t hash;
@@ -13,7 +15,9 @@ typedef struct ht_key_s{
 typedef struct ht_node_s{
   ht_key_t key;
   void* data;
+  list_head_t list;
   struct ht_node_s* next;
+  int used;
 }ht_node_t;
 
 typedef struct ht_table_s{ 
@@ -26,9 +30,9 @@ typedef struct ht_table_s{
 
 int ht_resize(ht_table_t* t);
 int ht_key_compare(ht_key_t k1,ht_key_t k2);
-int ht_set(ht_table_t* t,uint32_t k,void* v);
+int ht_set(ht_table_t* t,uint32_t k,void* v,ht_clean clean);
 void* ht_get(ht_table_t* t,uint32_t k);
-void* ht_remove(ht_table_t* t,uint32_t k);
+void ht_remove(ht_table_t* t,uint32_t k,ht_clean clean);
 
 
 #define ht_node_from(__src,__dst)		\
@@ -47,44 +51,45 @@ void* ht_remove(ht_table_t* t,uint32_t k);
   })
 
 
-static inline ht_table_t* ht_create()
+static inline void 
+ht_node_init(ht_node_t *node,uint32_t hash,uint32_t key,void *d)
 {
-  ht_table_t *t = pxy_calloc(sizeof(*t));
-  if(!t)goto failed;
-  t->len = HT_INIT_SIZE;
-  t->pool = mp_create(sizeof(ht_node_t),0,"HT");
-  if(!t->pool)goto failed;
-  t->nodes = mp_alloc(t->pool);
-  if(!t->nodes) goto failed;
+  node->key.key = key;
+  node->key.hash = hash;
+  node->data = d;
+  INIT_LIST_HEAD(&(node->list));
+}
 
+
+static inline ht_table_t* 
+ht_create()
+{
+  int i;
+  ht_table_t *t = malloc(sizeof(*t));
+  if(!t)
+    goto failed;
+
+  t->len = HT_INIT_SIZE;
+  t->nodes = pxy_calloc(sizeof(ht_node_t)*HT_INIT_SIZE);
+  if(!t->nodes) 
+    goto failed;
+
+  for (i=0; i<HT_INIT_SIZE; ++i) {
+    ht_node_init((ht_node_t*)(t->nodes)+i,0,0,NULL);
+  }
+  
   return t;
 
  failed:
-  if(t->pool)
-    mp_destroy(t->pool);
   if(t)
-    free(t);
+    FREE(t);
 
   return NULL;
 }
 
-static inline int ht_init(ht_table_t* t)
-{
-  if(t == NULL)
-    return -1;
-
-  t->len = HT_INIT_SIZE;
-  t->nodes = (ht_node_t**)calloc(1,sizeof(struct ht_node_s) 
-				 * HT_INIT_SIZE);
-  if(!t->nodes)
-    return -1;
-
-  return 0;
-}
-
-
 /*This algorith comes from google's snappy*/
-static inline uint32_t ht_hash_func(uint32_t bytes) 
+static inline uint32_t 
+ht_hash_func(uint32_t bytes) 
 {
   int shift = 5;
   uint32_t kMul = 0x1e35a7bd;
